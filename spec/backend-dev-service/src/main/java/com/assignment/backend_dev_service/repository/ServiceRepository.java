e3032b573f89d95b29b7ea20c7fb38916d98bea7
Hardik121020 <bansalhardik81@gmail.com>
2026-04-22 13:22:12 +0530

    Commit

--- spec/backend-dev-service/src/main/java/com/assignment/backend_dev_service/repository/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.backend_dev_service.repository;
-
-import com.assignment.backend_dev_service.model.ServiceMetadata;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceRepository extends JpaRepository<ServiceMetadata, String> {
-}
